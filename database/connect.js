5522b0d82efc92b707709141da82b9bd31496392
Khavan <krajput123@hotmail.com>
2025-08-14 01:21:42 +0100

    fix SQL

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,13 @@
-require("dotenv").config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const { Pool } = require("pg");
 
 const db = new Pool({
   connectionString: process.env.DB_URL,
+ 
 });
+
+console.log('DB_URL:', process.env.DB_URL);
 
 module.exports = db;
