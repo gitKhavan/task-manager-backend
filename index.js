bef500a81090f9a09a2bc361b09133c3e8853c63
Khavan <krajput123@hotmail.com>
2025-08-24 04:02:35 +0100

    test commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const { app } = require("./app");
+const http = require("http");
+
+
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log(`App running on port : ${port}`);
+})
