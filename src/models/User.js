db6b7dbd091b3245332b91f51475c857d5a56f07
Khavan <krajput123@hotmail.com>
2025-08-25 03:26:50 +0100

    create user routes

--- src/models/User.js
+++ src/models/User.js
@@ -2,9 +2,13 @@ const db = require("../../database/connect");
 
 class User{
     constructor({user_id, email, password}){
-        this.user_id = userId;
+        this.user_id = user_id;
         this.email = email;
         this.password = password;
     }
 
+    
+
 }
+
+module.exports = User;
