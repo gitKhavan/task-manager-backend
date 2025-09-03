c9a64ea50c8ce90e45a8a93b643e6b4a1d118ed6
Khavan <krajput123@hotmail.com>
2025-09-03 22:02:09 +0100

    create file for hashing and JWT auth

--- src/middleware/validators/validate.js
+++ src/middleware/validators/validate.js
@@ -1,10 +1,15 @@
-const { validationResult } = require("express-validator");
+const { validationResult, matchedData } = require("express-validator");
 
 const validate = (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()){
+        console.log("Validation errors:", errors.array())
         return res.status(400).json({ errors: errors.array()})
     }
+
+    const data = matchedData(req)
+    console.log(data)
+    
     next()
 }
 
