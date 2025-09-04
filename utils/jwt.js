8c9e6c7e9ccffb69475d73fa3382fb2495d40baa
Khavan <krajput123@hotmail.com>
2025-09-04 01:42:37 +0100

    create JWT token generation and verification

---
+++ utils/jwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function generateToken(user){
+    return jwt.sign(
+        { id: user.id, email: user.email }, 
+        JWT_SECRET, 
+        { expiresIn: '1h'})
+}
+
+function verifyToken(token){
+
+}
+
+module.exports = {
+    generateToken,
+    verifyToken
+}
